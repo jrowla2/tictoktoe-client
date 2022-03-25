bcd90ac369a9cd9b8a0445f09b681ce0cb9a5d21
jrowla2 <jrowla2@gmail.com>
2022-03-25 09:37:35 -0600

    Completed authorization
    Completed authorization and now am working on hide unauth and showing only the authorized game view.

---
+++ app/auth/events.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const authUi = require('./ui.js')
+const authApi = require('./api.js')
+const getFormFields = require('../../lib/get-form-fields.js')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  console.log('sign up')
+
+  // get data from form
+  const form = event.target
+  const data = getFormFields(form)
+  console.log(data)
+
+  authApi.signUp(data)
+    .then(() => authUi.onSignUpSuccess())
+    .catch(() => authUi.onSignUpFailure())
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+
+  // get data from form
+  const form = event.target
+  const data = getFormFields(form)
+  console.log(data)
+
+  authApi
+    .signIn(data)
+    .then((response) => authUi.onSignInSuccess(response))
+    .catch(() => authUi.onSignInFailure())
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+
+  // get data from form
+  const form = event.target
+  const data = getFormFields(form)
+  console.log(data)
+
+  authApi
+    .changePassword(data)
+    .then((response) => authUi.onChangePasswordSuccess(response))
+    .catch(() => authUi.onChangePasswordFailure())
+}
+
+const onSignOut = function () {
+  authApi
+    .signOut()
+    .then(() => authUi.onSignOutSuccess())
+    .catch(() => authUi.onSignOutFailure())
+}
+
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
---
+++ app/auth/ui.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const store = require('../store.js')
+
+const onSignUpSuccess = function () {
+  $('#auth-display').html('<p>User signed up successfully</p>')
+
+  $('form').trigger('reset')
+}
+
+const onSignUpFailure = function () {
+  $('#auth-display').html('<p>Error while signing up</p>')
+}
+
+const onSignInSuccess = function (response) {
+  $('#auth-display').html('<p>User signed in successfully</p>')
+
+  // reset all forms
+  $('form').trigger('reset')
+
+  console.log(response)
+  // store data from the response in my store object
+  store.user = response.user
+
+  // reset single form
+  // $('#sign-in-form').trigger('reset')
+
+  $('#unauth').hide
+}
+
+const onSignInFailure = function () {
+  $('#auth-display').html('<p>Error while signing in</p>')
+}
+
+const onChangePasswordSuccess = function () {
+  $('#auth-display').html('<p>User changed password successfully</p>')
+
+  $('form').trigger('reset')
+}
+
+const onChangePasswordFailure = function () {
+  $('#auth-display').html('<p>Error while changing password</p>')
+}
+
+const onSignOutSuccess = function () {
+  $('#auth-display').html('<p>User signed out successfully</p>')
+
+  $('form').trigger('reset')
+}
+
+const onSignOutFailure = function () {
+  $('#auth-display').html('<p>Error while signing out</p>')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
